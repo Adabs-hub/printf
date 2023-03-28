1049d7428d7730d2fdecc1a8b1297a24a7edf1c0
DeborahSarpong <dasarpong008@st.ug.edu.gh>
2023-03-28 12:49:17 -0700

    task

--- print_addresses.c
+++ print_addresses.c
@@ -7,7 +7,7 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_address(va_list l, flags_t *f)
+	int print_address(va_list l, flags_t *f)
 {
 	char *str;
 	unsigned long int p = va_arg(l, unsigned long int);
